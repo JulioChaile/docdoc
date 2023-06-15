5d4a6e41ee5af2c61a8379666963814b814af327
JulioChaile <65875066+JulioChaile@users.noreply.github.com>
2023-06-15 17:44:47 -0300

    update tables

--- db/tablas/Juzgados.sql
+++ db/tablas/Juzgados.sql
@@ -1,9 +1,10 @@
 CREATE TABLE `Juzgados` (
-  `IdJuzgado` int(11) NOT NULL,
-  `IdJurisdiccion` int(11) NOT NULL,
+  `IdJuzgado` int NOT NULL,
+  `IdJurisdiccion` int NOT NULL,
   `Juzgado` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Estado` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `ModoGestion` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Color` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT '000000',
   PRIMARY KEY (`IdJuzgado`,`IdJurisdiccion`),
   UNIQUE KEY `UI_Juzgado` (`Juzgado`),
   UNIQUE KEY `UI_IdJuzgado` (`IdJuzgado`),
--- db/tablas/TiposCaso.sql
+++ db/tablas/TiposCaso.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `TiposCaso` (
-  `IdTipoCaso` smallint(6) NOT NULL,
+  `IdTipoCaso` smallint NOT NULL,
   `TipoCaso` varchar(40) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `Estado` char(1) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Color` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT '000000',
   PRIMARY KEY (`IdTipoCaso`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
